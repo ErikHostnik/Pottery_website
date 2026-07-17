1e2da08d58a84700dc7f3088f423ab677fad9456
Erik Hostnik <erik.hostnik@gmail.com>
2026-07-17 17:42:31 +0200

    Added logo instead of text in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo-header.png'
 import './Header.css'
 
 export default function Header() {
   return (
     <header className="header">
-      <h1 className="header-title">Mica Studio</h1>
+      <NavLink to="/" className="header-logo-link">
+        <img className="header-logo" src={logo} alt="Mica Studio" />
+      </NavLink>
       <nav className="header-nav">
         <NavLink to="/">Domov</NavLink>
         <NavLink to="/about">O meni</NavLink>
